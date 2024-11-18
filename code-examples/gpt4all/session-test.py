c19b7d887ab9079c4248162ddf5646ff4b1b4f63
Dolica Akello-Egwel <d.akelloegwel@lcc.arts.ac.uk>
2024-11-18 16:44:29 +0000

    moving code around

--- code-examples/gpt4all/session-test.py
+++ code-examples/gpt4all/session-test.py
@@ -3,6 +3,7 @@ from gpt4all import GPT4All
 # choose a model to chat with - this will download it if it isn't already present on your machine
 model = GPT4All("orca-mini-3b-gguf2-q4_0.gguf")
 
-# start a chat session
+# have a conversation using a session rather than a one-off prompt
 with model.chat_session():
-    print(model.generate("Cats or dogs?", max_tokens=1024))
+    print(model.generate("Hello!"))
+    print(model.generate("Pick one: cats or dogs?"))
